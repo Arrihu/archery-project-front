f0d080e381581242fc7561e5969373424cf5ab5c
danielmuhamad <hamdanilm96@gmail.com>
2019-03-02 17:14:26 +0800

    feat: add bow route url

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import DefaultLayout from './containers/DefaultLayout';
 
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
 const Club = React.lazy(() => import('./components/club/Club'))
+const Bow = React.lazy(() => import('./components/bow/Bow'))
 const EventOrganizer = React.lazy(() => import('./components/event_organizer/EventOrganizer'))
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
@@ -10,6 +11,7 @@ const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/club', name: 'Club', component: Club },
+  { path: '/bow', name: 'Bow', component: Bow },
   { path: '/event_organizer', name: 'Event Organizer', component: EventOrganizer }
 ];
 
