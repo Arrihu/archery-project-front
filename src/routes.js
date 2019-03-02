82fd41844c7849569aa93c0a8f0866dcd342c6e9
danielmuhamad <hamdanilm96@gmail.com>
2019-03-02 14:23:31 +0800

    chore: adding event organizer component route

--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import DefaultLayout from './containers/DefaultLayout';
 
 const Dashboard = React.lazy(() => import('./views/Dashboard'));
-const Club = React.lazy(() => import('./components/Club/Club'))
+const Club = React.lazy(() => import('./components/club/Club'))
+const EventOrganizer = React.lazy(() => import('./components/event_organizer/EventOrganizer'))
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/club', name: 'Club', component: Club },
+  { path: '/event_organizer', name: 'Event Organizer', component: EventOrganizer }
 ];
 
 export default routes;
