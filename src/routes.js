73e1700e0ab529918cb65e52bb6415fbe5eaf586
danielmuhamad <hamdanilm96@gmail.com>
2019-03-02 18:09:01 +0800

    chore: add arrow route url

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import DefaultLayout from './containers/DefaultLayout';
 
-const Dashboard = React.lazy(() => import('./views/Dashboard'));
+const Dashboard = React.lazy(() => import('./views/Dashboard'))
 const Club = React.lazy(() => import('./components/club/Club'))
 const Bow = React.lazy(() => import('./components/bow/Bow'))
+const Arrow = React.lazy(() => import('./components/arrow/Arrow'))
 const EventOrganizer = React.lazy(() => import('./components/event_organizer/EventOrganizer'))
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
@@ -12,6 +13,7 @@ const routes = [
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/club', name: 'Club', component: Club },
   { path: '/bow', name: 'Bow', component: Bow },
+  { path: '/arrow', name: 'Arrow', component: Arrow },
   { path: '/event_organizer', name: 'Event Organizer', component: EventOrganizer }
 ];
 
