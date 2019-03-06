f2800de23730fca649b928510e46977693c716bb
danielmuhamad <hamdanilm96@gmail.com>
2019-03-06 15:01:34 +0800

    chore: adding tournament route url

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const Arrow = React.lazy(() => import('./components/arrow/Arrow'))
 const EventOrganizer = React.lazy(() => import('./components/event_organizer/EventOrganizer'))
 const User = React.lazy(() => import('./components/user/User'))
 const Role = React.lazy(() => import('./components/role/Role'))
+const Tournament = React.lazy(() => import('./components/tournament/Tournament'))
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
@@ -17,6 +18,7 @@ const routes = [
   { path: '/data-master/bow', name: 'Bow', component: Bow },
   { path: '/data-master/arrow', name: 'Arrow', component: Arrow },
   { path: '/data-master/event_organizer', name: 'Event Organizer', component: EventOrganizer },
+  { path: '/data-master/tournament', name: 'Tournament', component: Tournament },
   { path: '/user', name: 'User', component: User },
   { path: '/role', name: 'Role', component: Role },
 ];
