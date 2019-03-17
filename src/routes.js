c3bf9edb5af4f1281a9a81794ab658833ca81329
danielmuhamad <hamdanilm96@gmail.com>
2019-03-17 18:06:38 +0800

    chore: update tournament url route

--- src/routes.js
+++ src/routes.js
@@ -14,11 +14,11 @@ const Tournament = React.lazy(() => import('./components/tournament/Tournament')
 const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
+  { path: '/tournament', name: 'Tournament', component: Tournament },
   { path: '/data-master/club', name: 'Club', component: Club },
   { path: '/data-master/bow', name: 'Bow', component: Bow },
   { path: '/data-master/arrow', name: 'Arrow', component: Arrow },
   { path: '/data-master/event_organizer', name: 'Event Organizer', component: EventOrganizer },
-  { path: '/data-master/tournament', name: 'Tournament', component: Tournament },
   { path: '/user', name: 'User', component: User },
   { path: '/role', name: 'Role', component: Role },
 ];
