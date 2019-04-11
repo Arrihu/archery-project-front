9cedd0702d37a9cee32e899fdbaab1bbb98d621e
ahadsenja <hamdanilm96@gmail.com>
2019-04-11 14:04:15 +0800

    chore: adding filed placeholder

--- src/app/User/UserForm.jsx
+++ src/app/User/UserForm.jsx
@@ -20,7 +20,7 @@ class UserForm extends React.Component {
                                 <div className="form-group">
                                     <label>Nama</label>
                                     <input name="name" type="text" className="form-control" 
-                                    value={name} onChange={this.props.handleChange}></input>
+                                    value={name} onChange={this.props.handleChange} placeholder='Input name'></input>
                                 </div>
                             </Col>
 
@@ -28,7 +28,7 @@ class UserForm extends React.Component {
                                 <div className="form-group">
                                     <label>Email</label>
                                     <input name="email" type="email" className="form-control"
-                                    value={email} onChange={this.props.handleChange}></input>
+                                    value={email} onChange={this.props.handleChange} placeholder='Email address'></input>
                                 </div>
                             </Col>
 
@@ -36,7 +36,7 @@ class UserForm extends React.Component {
                                 <div className="form-group">
                                     <label>Username</label>
                                     <input name="username" type="test" className="form-control"
-                                    value={username} onChange={this.props.handleChange}></input>
+                                    value={username} onChange={this.props.handleChange} placeholder='Username'></input>
                                 </div>
                             </Col>
 
@@ -44,17 +44,17 @@ class UserForm extends React.Component {
                                 <div className="form-group">
                                     <label>Password</label>
                                     <input name="password" type="password" className="form-control"
-                                    value={password} onChange={this.props.handleChange}></input>
+                                    value={password} onChange={this.props.handleChange} placeholder='Password'></input>
                                 </div>
                             </Col>
                         </Row>
                     </form>
 
-                    <Button className='btn-save-user' type="submit" color="primary" data-dismiss="modal" 
+                    <Button className='btn-save-user' type="submit" color="primary"
                     onClick={mode === "update" ? this.props.onUpdateUser : this.props.onCreateUser}
                     >Save</Button>
                     <span className='user-btn-space'></span>
-                    <Button color="secondary" onClick={this.props.modalToggle}>Cancel</Button>
+                    <Button color="secondary">Cancel</Button>
                 </Card>
             </div>
         )
