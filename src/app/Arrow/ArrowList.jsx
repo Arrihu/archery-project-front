f6058af6543cc52b4c6eb088a17e4e63489cbda7
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 15:33:19 +0800

    chore: update arrow list data table and action button issue #3

--- src/app/Arrow/ArrowList.jsx
+++ src/app/Arrow/ArrowList.jsx
@@ -1,45 +1,72 @@
 import React from 'react'
-import { Table, Button } from 'reactstrap'
+import { Table, Button, Card, Row, Col } from 'reactstrap'
 
 
 class ArrowList extends React.Component {
 
     render() {
+        const { arrows } = this.props.dataState
+
         return (
             <div>
-                <div className="card">
+                <Card>
+                    <Row>
+                        <Col>
+                            <div className='list-arrow-title'>
+                                <h4><b>List Arrow Data</b></h4>
+                            </div>
+                        </Col>
+
+                        <Col>
+                            <div className='btn-add-arrow' align='right'>
+                                <Button color='primary' onClick={this.props.toggleArrowForm}>
+                                    <i className='cui-cursor'></i> <b>Add Arrow</b>
+                                </Button>
+                            </div>
+                        </Col>
+                    </Row>
+
                     <Table>
                         <thead>
                             <tr>
-                                <th>No</th>
+                                <th>#</th>
                                 <th>Arrow Type</th>
                                 <th>Length</th>
                                 <th>Diameter</th>
+                                <th>Spine</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
 
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Platinum</td>
-                                <td>80 Cm</td>
-                                <td>5 mm</td>
-                                <td>
-                                    <Button color="warning" size="sm">
-                                        <i className="cui-note"></i>
-                                    </Button>
+                            {
+                                typeof(arrows) === 'object' &&
+                                arrows.map((arrow, i) => {
+                                    return (
+                                        <tr key={i}>
+                                            <td>{i+1}</td>
+                                            <td>{arrow.type}</td>
+                                            <td>{arrow.length}</td>
+                                            <td>{arrow.diameter}</td>
+                                            <td>{arrow.spine}</td>
+                                            <td>
+                                                <Button color="warning" size="sm" onClick={this.props.selectArrow.bind(this, arrow)}>
+                                                    <i className="cui-note"></i>
+                                                </Button>
 
-                                    <span style={{ paddingLeft: 5 }}></span>
+                                                <span style={{ paddingLeft: 5 }}></span>
 
-                                    <Button color="danger" size="sm">
-                                        <i className="cui-trash"></i>
-                                    </Button>
-                                </td>
-                            </tr>
+                                                <Button color="danger" size="sm" onClick={this.props.onDeleteArrow.bind(this, arrow.id)}>
+                                                    <i className="cui-trash"></i>
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    )
+                                })
+                            }
                         </tbody>
                     </Table>
-                </div>
+                </Card>
             </div>
         )
     }
