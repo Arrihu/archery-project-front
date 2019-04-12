adcc363580b96dda2a28c201161115898504e24e
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 15:32:34 +0800

    chore: update arrow form issue #6

--- src/app/Arrow/ArrowForm.jsx
+++ src/app/Arrow/ArrowForm.jsx
@@ -7,40 +7,52 @@ import './style/arrow.scss'
 class ArrowForm extends React.Component {
 
     render() {
+        const { type, length, diameter, spine, mode } = this.props.dataState
+
         return (
             <div>
                 <Card className='form-card'>
-                    <h5><strong>Add Arrow Data</strong></h5>
-                    
-                    <form action="">
-                        <br/>
+                    {/* title */}
+                    <h5><strong>Arrow Data</strong></h5>
+                    {/* arrow form */}
+                    <form onSubmit={mode === 'update' ? this.props.onUpdateArrow : this.props.onCreateArrow}>
                         <Row>
                             <Col>
                                 <div className="form-group">
-                                    <label htmlFor="arrowType">Arrow Type</label>
-                                    <input type="text" className="form-control" placeholder="Arrow type" />
+                                    <label htmlFor="arrowType">Type</label>
+                                    <input type="text" name='type' value={type} onChange={this.props.handleChange} className="form-control" placeholder="Arrow type" />
                                 </div>
                             </Col>
 
                             <Col>
                                 <div className="form-group">
                                     <label htmlFor="arrowLength">Length</label>
-                                    <input type="text" className="form-control" placeholder="Length" />
+                                    <input type="text" name='length' value={length} onChange={this.props.handleChange} className="form-control" placeholder="Length" />
                                 </div>
                             </Col>
 
                             <Col>
                                 <div className="form-group">
                                     <label htmlFor="arrowDiameter">Diameter</label>
-                                    <input type="text" className="form-control" placeholder="Diameter" />
+                                    <input type="text" name='diameter' value={diameter} onChange={this.props.handleChange} className="form-control" placeholder="Diameter" />
+                                </div>
+                            </Col>
+
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="arrowDiameter">Spine</label>
+                                    <input type="text" name='spine' value={spine} onChange={this.props.handleChange} className="form-control" placeholder="Spine" />
                                 </div>
                             </Col>
                         </Row>
                     </form>
 
-                    <Button className='btn-save' color="primary">Save</Button>
+                    {/* button action */}
+                    <Button className='btn-save' type='submit' color="primary" onClick={mode === 'update' ? this.props.onUpdateArrow : this.props.onCreateArrow}><b>Save</b></Button>
+
                     <span className='btn-space'></span>
-                    <Button color="secondary" onClick={this.props.dataToggle}>Cancel</Button>
+
+                    <Button color="secondary" onClick={this.props.toggleArrowForm}><b>Cancel</b></Button>
                 </Card>
             </div>
         )
