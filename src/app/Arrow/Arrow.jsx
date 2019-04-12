339eac92daaf10b9b62e1c34ef47b37011ab1791
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 15:30:57 +0800

    feat: add oncreate, onupdate, ondelete arrow function issue #9

--- src/app/Arrow/Arrow.jsx
+++ src/app/Arrow/Arrow.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import axios from 'axios'
 
 import ArrowForm from './ArrowForm'
 import ArrowList from './ArrowList'
+import Axios from 'axios';
 
 
 class Arrow extends React.Component {
@@ -10,23 +12,155 @@ class Arrow extends React.Component {
         super(props)
         this.state = {
             id: '',
-            arrow_type: '',
+            type: '',
             length: '',
-            diameter: ''
+            diameter: '',
+            spine: '',
+
+            arrows: [],
+            mode: 'add',
+            showArrowForm: false
         }
+    }
+
+    componentDidMount() {
+        this.listAllArrow()
+    }
+
+    toggleArrowForm = () => {
+        this.setState({
+            showArrowForm: !this.state.showArrowForm
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    resetForm = () => {
+        this.setState({
+            id: '',
+            type: '',
+            length: '',
+            diameter: '',
+            spine: ''
+        })
+    }
+
+    selectArrow = (arrow) => {
+        this.setState({
+            id: arrow.id,
+            type: arrow.type,
+            length: arrow.length,
+            diameter: arrow.diameter,
+            spine: arrow.spine,
+            mode: 'update'
+        })
+        this.toggleArrowForm()
+    }
+
+    addArrow = (arrow) => {
+        const { arrows } = this.state
+        arrows.push(arrow)
+        this.setState({
+            arrows
+        })
+    }
+
+    updateArrow = (arrow) => {
+        const { arrows } = this.state
+        let index = arrows.findIndex((data) => data.id === arrow.id)
+        arrows[index] = arrow
+        this.setState({
+            arrows: arrows,
+            mode: 'add'
+        })
+        this.toggleArrowForm()
+    }
+
+    deleteArrow = (id) => {
+        const { arrows } = this.state
+        let temp = arrows.filter(data => data.id !== id)
+        this.setState({
+            arrows: temp
+        })
+    }
+
+    listAllArrow = () => {
+        axios.get('http://localhost:3333/arrow')
+            .then((result) => {
+                this.setState({
+                    arrows: result.data.data
+                })
+            })
+    }
+
+    onCreateArrow = (event) => {
+        event.preventDefault()
+
+        const arrow = {
+            id: this.state.id,
+            type: this.state.type,
+            length: this.state.length,
+            diameter: this.state.diameter,
+            spine: this.state.spine
+        }
+        axios.post('http://localhost:3333/arrow/create', arrow)
+            .then(res => res.data)
+            .then(res => this.addArrow(res.data))
+
+        this.resetForm()
+        this.toggleArrowForm()
+    }
+
+    onUpdateArrow = (event) => {
+        event.preventDefault()
+
+        const arrow = {
+            id: this.state.id,
+            type: this.state.type,
+            length: this.state.length,
+            diameter: this.state.diameter,
+            spine: this.state.spine
+        }
+
+        axios.post('http://localhost:3333/arrow/update', arrow)
+            .then(res => res.data)
+            .then(res => this.updateArrow(res.data))
+
+        this.resetForm()
+    }
+
+    onDeleteArrow = (id) => {
+        axios.get(`http://localhost:3333/arrow/delete/${id}`)
+            .then(res => res.data)
+            .then(res => this.deleteArrow(id))
     }
 
     render() {
         return (
             <div>
-                <ArrowForm
-                    dataState={this.state}
-                    dataToggle={this.toggle}                
-                ></ArrowForm>
+                {this.state.showArrowForm &&
+                    <ArrowForm
+                        dataState={this.state}
+                        toggleArrowForm={this.toggleArrowForm}
+                        handleChange={this.handleChange}    
+                        onCreateArrow={this.onCreateArrow}    
+                        onUpdateArrow={this.onUpdateArrow}     
+                    />
+                }
 
-                <ArrowList
-                    dataToggle={this.toggle}
-                ></ArrowList>
+                {!this.state.showArrowForm &&
+                    <ArrowList
+                        dataState={this.state}
+                        toggleArrowForm={this.toggleArrowForm}
+                        onCreateArrow={this.onCreateArrow}
+                        selectArrow={this.selectArrow}
+                        onDeleteArrow={this.onDeleteArrow}
+                    />
+                }
             </div>
         )
     }
