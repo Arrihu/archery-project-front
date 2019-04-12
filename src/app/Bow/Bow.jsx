d5b065c893d45ba2afdd26c69d59684acb8bfe4e
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 18:00:03 +0800

    feat: add bow crud operation issue #10

--- src/app/Bow/Bow.jsx
+++ src/app/Bow/Bow.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios';
 
 import BowForm from './BowForm'
 import BowList from './BowList'
@@ -9,29 +10,160 @@ class Bow extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            modal: false
-        }
+            id: '',
+            type: '',
+            length: '',
+            weight: '',
 
-        this.toggle = this.toggle.bind(this)
+            bows: [],
+            mode: 'add',
+            showBowForm: false
+        }
     }
 
-    toggle() {
+    componentDidMount() {
+        this.listAllBow()
+    }
+
+    toggleBowForm = () => {
         this.setState({
-            modal: !this.state.modal
+            showBowForm: !this.state.showBowForm
         })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    resetForm = () => {
+        this.setState({
+            id: '',
+            type: '',
+            length: '',
+            weight: ''
+        })
+    } 
+
+    cancelAction = () => {
+        this.resetForm()
+        this.toggleBowForm()
+    }
+
+    selectBow = (bow) => {
+        this.setState({
+            id: bow.id,
+            type: bow.type,
+            length: bow.length,
+            weight: bow.weight,
+            mode: 'update'
+        })
+
+        this.toggleBowForm()
+    }
+
+    addBow = (bow) => {
+        const { bows } = this.state
+        bows.push(bow)
+        this.setState({
+            bows
+        })
+    }
+
+    updateBow = (bow) => {
+        const { bows } = this.state
+        let index = bows.findIndex((data) => data.id === bow.id)
+        bows[index] = bow
+
+        this.setState({
+            bows: bows,
+            mode: 'add'
+        })
+        
+        this.toggleBowForm()
+    }
+
+    deleteBow = (id) => {
+        const { bows } = this.state
+        let temp = bows.filter(data => data.id !== id)
+        this.setState({
+            bows: temp
+        })
+    }
+
+    listAllBow = () => {
+        axios.get('http://localhost:3333/bow')
+            .then((result) => {
+                this.setState({
+                    bows: result.data.data
+                })
+            })
+    }
+
+    onCreateBow = (event) => {
+        event.preventDefault()
+
+        const bow = {
+            id: this.state.id,
+            type: this.state.type,
+            length: this.state.length,
+            weight: this.state.weight
+        }
+
+        axios.post('http://localhost:3333/bow/create', bow)
+            .then(res => res.data)
+            .then(res => this.addBow(res.data))
+        
+        this.resetForm()
+        this.toggleBowForm()
+    }
+
+    onUpdateBow = (event) => {
+        event.preventDefault()
+
+        const bow = {
+            id: this.state.id,
+            type: this.state.type,
+            length: this.state.length,
+            weight: this.state.weight
+        }
+
+        axios.post('http://localhost:3333/bow/update', bow)
+            .then(res => res.data)
+            .then(res => this.updateBow(res.data))
+        
+        this.resetForm()
+    }
+
+    onDeleteBow = (id) => {
+        axios.get(`http://localhost:3333/bow/delete/${id}`)
+            .then(res => res.data)
+            .then(res => this.deleteBow(id))
     }
 
     render() {
         return (
             <div>
-                <BowForm 
-                    dataState={this.state}
-                    dataToggle={this.toggle}
-                ></BowForm>
+                {this.state.showBowForm &&
+                    <BowForm 
+                        dataState={this.state}
+                        handleChange={this.handleChange}
+                        toggleBowForm={this.toggleBowForm}
+                        onCreateBow={this.onCreateBow}
+                        onUpdateBow={this.onUpdateBow}
+                        cancelAction={this.cancelAction}
+                    />
+                }
 
-                <BowList
-                    dataToggle={this.toggle}
-                ></BowList>
+                {!this.state.showBowForm &&
+                    <BowList
+                        dataState={this.state}
+                        toggleBowForm={this.toggleBowForm}
+                        selectBow={this.selectBow}
+                        onDeleteBow={this.onDeleteBow}
+                    />
+                }
             </div>
         )
     }
