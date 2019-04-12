e7ee049240c567c98c2dcf2124f1fca29ccd3f55
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 18:02:57 +0800

    chore: update bow form issue  #7

--- src/app/Bow/BowForm.jsx
+++ src/app/Bow/BowForm.jsx
@@ -1,41 +1,52 @@
 import React from 'react'
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap'
+import { Button, Card, Row, Col } from 'reactstrap'
+
+import './style/bow.scss'
 
 
 class BowForm extends React.Component {
 
     render() {
-        const { modal } = this.props.dataState
+        const { type, length, weight, mode } = this.props.dataState
 
         return (
             <div>
-                <Modal isOpen={modal} toggle={this.props.dataToggle}>
-                    <ModalHeader toggle={this.props.dataToggle}>Bow Data</ModalHeader>
+                <Card className='bow-form-card'>
+                    {/* title */}
+                    <h5><b>Bow Data Form</b></h5>
+                    {/* bow form */}
+                    <form onSubmit={mode === 'update' ? this.props.onUpdateBow : this.props.onCreateBow}>
+                        <Row>
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="bowType">Bow Type</label>
+                                    <input type="text" name='type' value={type} onChange={this.props.handleChange} className="form-control" placeholder="Bow type" />
+                                </div>
+                            </Col>
 
-                    <ModalBody>
-                        <form action="">
-                            <div className="form-group">
-                                <label htmlFor="bowType">Bow Type</label>
-                                <input type="text" className="form-control" placeholder="Bow type" />
-                            </div>
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="drawLength">Bow Length</label>
+                                    <input type="text" name='length' value={length} onChange={this.props.handleChange} className="form-control" placeholder="Draw length" />
+                                </div>
+                            </Col>
 
-                            <div className="form-group">
-                                <label htmlFor="drawLength">Draw Length</label>
-                                <input type="text" className="form-control" placeholder="Draw length" />
-                            </div>
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="drawWeight">Bow Weight</label>
+                                    <input type="text" name='weight' value={weight} onChange={this.props.handleChange} className="form-control" placeholder="Draw weight" />
+                                </div>
+                            </Col>
+                        </Row>
+                    </form>
 
-                            <div className="form-group">
-                                <label htmlFor="drawWeight">Draw Weight</label>
-                                <input type="text" className="form-control" placeholder="Draw weight" />
-                            </div>
-                        </form>
-                    </ModalBody>
+                    {/* action button */}
+                    <Button color="primary" onClick={mode === 'update' ? this.props.onUpdateBow : this.props.onCreateBow}>Save</Button>
 
-                    <ModalFooter>
-                        <Button color="primary">Save</Button>
-                        <Button color="secondary" onClick={this.props.dataToggle}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
+                    <span className='btn-space-bow'></span>
+
+                    <Button color="secondary" onClick={this.props.cancelAction}>Cancel</Button>
+                </Card>
             </div>
         )
     }
