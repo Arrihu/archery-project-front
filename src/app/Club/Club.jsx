a6e6c074c1f0da1a208d03be34f780dcce3c4810
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 20:21:40 +0800

    feat: add club crud operation issue #11

--- src/app/Club/Club.jsx
+++ src/app/Club/Club.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import axios from 'axios'
+
 import ClubForm from './ClubForm'
 import ClubList from './ClubList'
 
@@ -8,30 +10,156 @@ class Club extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            modal: false
-        }
+            id: '',
+            name: '',
+            address: '',
 
-        this.toggle = this.toggle.bind(this)
+            clubs: [],
+            mode: 'add',
+            showClubForm: false
+        }
     }
 
-    toggle() {
+    componentDidMount() {
+        this.listAllClub()
+    }
+
+    toggleClubForm = () => {
         this.setState({
-            modal: !this.state.modal
+            showClubForm: !this.state.showClubForm
         })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    resetForm = () => {
+        this.setState({
+            id: '',
+            name: '',
+            address: ''
+        })
+    }
+
+    onCancel = () => {
+        this.resetForm()
+        this.toggleClubForm()
+    }
+
+    selectClub = (club) => {
+        this.setState({
+            id: club.id,
+            name: club.name,
+            address: club.address,
+            mode: 'update'
+        })
+
+        this.toggleClubForm()
+    }
+
+    addClub = (club) => {
+        const { clubs } = this.state
+        clubs.push(club)
+        this.setState({
+            clubs
+        })
+
+        this.resetForm()
+    }
+
+    updateClub = (club) => {
+        const { clubs } = this.state
+        let index = clubs.findIndex((data) => data.id === club.id)
+        clubs[index] = club
+
+        this.setState({
+            clubs
+        })
+    }
+
+    deleteClub = (id) => {
+        const { clubs } = this.state
+        let temp = clubs.filter(data => data.id !== id)
+
+        this.setState({
+            clubs: temp
+        })
+    }
+
+    listAllClub = () => {
+        axios.get('http://localhost:3333/club')
+            .then((result) => {
+                this.setState({
+                    clubs: result.data.data
+                })
+            })
+    }
+
+    onCreateClub = (event) => {
+        event.preventDefault()
+
+        const club = {
+            id: this.state.id,
+            name: this.state.name,
+            address: this.state.address
+        }
+
+        axios.post('http://localhost:3333/club/create', club)
+            .then(res => res.data)
+            .then(res => this.addClub(res.data))
+
+        this.resetForm()
+        this.toggleClubForm()
+    }
+
+    onUpdateClub = (event) => {
+        event.preventDefault()
+
+        const club = {
+            id: this.state.id,
+            name: this.state.name,
+            address: this.state.address
+        }
+
+        axios.post('http://localhost:3333/club/update', club)
+            .then(res => res.data)
+            .then(res => this.updateClub(res.data))
+
+        this.resetForm()
+        this.toggleClubForm()
+    }
+
+    onDeleteClub = (id) => {
+        axios.get(`http://localhost:3333/club/delete/${id}`)
+            .then(res => res.data)
+            .then(res => this.deleteClub(id))
     }
 
     render() {
         return (
             <div>
-                <ClubForm
-                    dataState={this.state}
-                    modalToggle={this.toggle}
-                >
-                </ClubForm>
-                <ClubList
-                    modalToggle={this.toggle}
-                >
-                </ClubList>
+                {this.state.showClubForm &&
+                    <ClubForm
+                        dataState={this.state}
+                        toggleClubForm={this.toggleClubForm}
+                        handleChange={this.handleChange}
+                        onCreateClub={this.onCreateClub}
+                        onUpdateClub={this.onUpdateClub}
+                        onCancel={this.onCancel}
+                    />
+                }
+
+                {!this.state.showClubForm &&
+                    <ClubList
+                        dataState={this.state}
+                        toggleClubForm={this.toggleClubForm}
+                        selectClub={this.selectClub}
+                        onDeleteClub={this.onDeleteClub}
+                    />
+                }
             </div>
         )
     }
