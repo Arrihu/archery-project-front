8089a65c09e350306e9e3278fcc0a46e944eb9ad
ahadsenja <hamdanilm96@gmail.com>
2019-04-13 01:19:29 +0800

    chore: update home page url import and route

--- src/App.js
+++ src/App.js
@@ -4,22 +4,29 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import Loadable from 'react-loadable';
 import './App.scss';
 
+// loading
 const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
-
 // Containers
 const DefaultLayout = Loadable({
   loader: () => import('./containers/DefaultLayout'),
   loading
 });
 
+// pages
+const HomePage = React.lazy(() => import('./app/HomePage/HomePage'))
+
+
 class App extends Component {
 
   render() {
     return (
       <HashRouter>
+        <React.Suspense fallback={loading()}>
           <Switch>
+            <Route exact path="/home" name="Home Page" render={props => <HomePage {...props}/>} />
             <Route path="/" name="Home" component={DefaultLayout} />
           </Switch>
+        </React.Suspense>
       </HashRouter>
     );
   }
