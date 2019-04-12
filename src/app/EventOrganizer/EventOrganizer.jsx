51fca9667f9f0b68236b93a719ca174c71777726
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 21:24:19 +0800

    chore: update function style to es6 and adding on cancel action function issue #18

--- src/app/EventOrganizer/EventOrganizer.jsx
+++ src/app/EventOrganizer/EventOrganizer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+
 import EventOrganizerForm from './EventOrganizerForm'
 import EventOrganizerList from './EventOrganizerList'
 
@@ -12,41 +13,30 @@ class EventOrganizer extends React.Component {
       id: '',
       position: '',
       description: '',
-      event_organizers: [],
-      modal: false,
-      mode: 'add'
-    }
 
-    this.toggle = this.toggle.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.resetForm = this.resetForm.bind(this)
-    this.selectEventOrganizer = this.selectEventOrganizer.bind(this)
-    this.addEventOrganizer = this.addEventOrganizer.bind(this)
-    this.updateEventOrganizer = this.updateEventOrganizer.bind(this)
-    this.deleteEventOrganizer = this.deleteEventOrganizer.bind(this)
-    this.listAllEventOrganizer = this.listAllEventOrganizer.bind(this)
-    this.onCreateEventOrganizer = this.onCreateEventOrganizer.bind(this)
-    this.onUpdateEventOrganizer = this.onUpdateEventOrganizer.bind(this)
-    this.onDeleteEventOrganizer = this.onDeleteEventOrganizer.bind(this)
+      event_organizers: [],
+      mode: 'add',
+      showEventOrganizerForm: false
+    }
   }
 
   componentDidMount() {
     this.listAllEventOrganizer()
   }
 
-  toggle() {
+  toggleEventOrganizerForm = () => {
     this.setState({
-      modal: !this.state.modal
+      showEventOrganizerForm: !this.state.showEventOrganizerForm
     })
   }
 
-  handleChange(event) {
+  handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value
     })
   }
 
-  resetForm() {
+  resetForm = () => {
     this.setState({
       id: '',
       position: '',
@@ -54,46 +44,48 @@ class EventOrganizer extends React.Component {
     })
   }
 
-  selectEventOrganizer(event_organizer) {
+  onCancel = () => {
+    this.resetForm()
+    this.toggleEventOrganizerForm()
+  }
+
+  selectEventOrganizer = (eo) => {
     this.setState({
-      id: event_organizer.id,
-      position: event_organizer.position,
-      description: event_organizer.description,
+      id: eo.id,
+      position: eo.position,
+      description: eo.description,
       mode: 'update'
     })
 
-    this.toggle()
+    this.toggleEventOrganizerForm()
   }
 
-  addEventOrganizer(event_organizer) {
+  addEventOrganizer = (eo) => {
     const { event_organizers } = this.state
-
-    event_organizers.push(event_organizer)
+    event_organizers.push(eo)
     this.setState({ event_organizers })
   }
 
-  updateEventOrganizer(event_organizer) {
+  updateEventOrganizer = (eo) => {
     const { event_organizers } = this.state
-    let index = event_organizers.findIndex(data => data.id === event_organizer.id)
+    let index = event_organizers.findIndex(data => data.id === eo.id)
+    event_organizers[index] = eo
 
-    event_organizers[index] = event_organizer
     this.setState({
       event_organizers,
       mode: 'add'
     })
   }
 
-  deleteEventOrganizer(id) {
+  deleteEventOrganizer = (id) => {
     const { event_organizers } = this.state
     let temp = event_organizers.filter(data => data.id !== id)
 
     this.setState({ event_organizers: temp })
   }
 
-  listAllEventOrganizer() {
-    const URL = "http://localhost:3002/panitia"
-    
-    axios.get(URL)
+  listAllEventOrganizer = () => {    
+    axios.get('http://localhost:3333/event-organizer')
       .then((res) => {
         this.setState({
           event_organizers: res.data.data
@@ -101,46 +93,42 @@ class EventOrganizer extends React.Component {
       })
   }
 
-  onCreateEventOrganizer(event) {
+  onCreateEventOrganizer = (event) => {
     event.preventDefault()
 
-    const URL = "http://localhost:3002/panitia/create"
-    const event_organizer = {
+    const eo = {
       id: this.state.id,
       position: this.state.position,
       description: this.state.description,
     }
 
-    axios.post(URL, event_organizer)
+    axios.post('http://localhost:3333/event-organizer/create', eo)
       .then(res => res.data)
       .then(res => this.addEventOrganizer(res.data))
 
     this.resetForm()
-    this.toggle()
+    this.toggleEventOrganizerForm()
   }
 
-  onUpdateEventOrganizer(event) {
+  onUpdateEventOrganizer = (event) => {
     event.preventDefault()
     
-    const URL = "http://localhost:3002/panitia/update"
-    const event_organizer = {
+    const eo = {
       id: this.state.id,
       position: this.state.position,
       description: this.state.description,
     }
 
-    axios.post(URL, event_organizer)
+    axios.post('http://localhost:3333/event-organizer/update', eo)
       .then(res => res.data)
       .then(res => this.updateEventOrganizer(res.data))
 
     this.resetForm()
-    this.toggle()
+    this.toggleEventOrganizerForm()
   }
 
-  onDeleteEventOrganizer(id) {
-    const URL = `http://localhost:3002/panitia/delete/${id}`
-
-    axios.get(URL)
+  onDeleteEventOrganizer = (id) => {
+    axios.get(`http://localhost:3333/event-organizer/delete/${id}`)
       .then(res => res.data)
       .then(res => this.deleteEventOrganizer(id))
   }
@@ -148,22 +136,25 @@ class EventOrganizer extends React.Component {
   render() {
     return (
       <div>
-        <EventOrganizerForm
-          dataState={this.state}
-          handleChange={this.handleChange}
-          modalToggle={this.toggle}
-          onCreateEventOrganizer={this.onCreateEventOrganizer}
-          onUpdateEventOrganizer={this.onUpdateEventOrganizer}
-        >
-        </EventOrganizerForm>
+        {this.state.showEventOrganizerForm &&
+          <EventOrganizerForm
+            dataState={this.state}
+            handleChange={this.handleChange}
+            toggleEventOrganizerForm={this.toggleEventOrganizerForm}
+            onCreateEventOrganizer={this.onCreateEventOrganizer}
+            onUpdateEventOrganizer={this.onUpdateEventOrganizer}
+            onCancel={this.onCancel}
+          />
+        }
 
-        <EventOrganizerList
-          dataState={this.state}
-          modalToggle={this.toggle}
-          selectEventOrganizer={this.selectEventOrganizer}
-          onDeleteEventOrganizer={this.onDeleteEventOrganizer}
-        >
-        </EventOrganizerList>        
+        {!this.state.showEventOrganizerForm &&
+          <EventOrganizerList
+            dataState={this.state}
+            toggleEventOrganizerForm={this.toggleEventOrganizerForm}
+            selectEventOrganizer={this.selectEventOrganizer}
+            onDeleteEventOrganizer={this.onDeleteEventOrganizer}
+          />
+        }      
       </div>
     )
   }
