1638805335e9963d42deb3a6d399aadc1746988e
ahadsenja <hamdanilm96@gmail.com>
2019-04-12 20:22:46 +0800

    chore: update club form issue #8

--- src/app/Club/ClubForm.jsx
+++ src/app/Club/ClubForm.jsx
@@ -1,37 +1,45 @@
 import React from 'react'
-import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap'
+import { Button, Card, Row, Col } from 'reactstrap'
+
+import './style/club.scss'
 
 
 class ClubForm extends React.Component {
 
     render() {
-
-        const { modal } = this.props.dataState
+        const { name, address, mode } = this.props.dataState
 
         return (
             <div>
-                <Modal isOpen={modal} toggle={this.props.modalToggle}>
-                    <ModalHeader toggle={this.props.modalToggle}>Add Data Club</ModalHeader>
+                <Card className='club-form-card'>
+                    {/* title */}
+                    <h5><b>Club Data Form</b></h5>
+                    {/* club form */}
+                    <form onSubmit={mode === 'update' ? this.props.onUpdateClub : this.props.onCreateClub}>
+                        <Row>
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="clubName">Club</label>
+                                    <input type="text" name='name' value={name} onChange={this.props.handleChange} className="form-control" placeholder="Club name" />
+                                </div>
+                            </Col>
 
-                    <ModalBody>
-                        <form action="">
-                            <div className="form-group">
-                                <label htmlFor="clubName">Club</label>
-                                <input type="text" className="form-control" placeholder="Club name" />
-                            </div>
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="clubAddress">Address</label>
+                                    <input type="text" name='address' value={address} onChange={this.props.handleChange} className="form-control" placeholder="Address" />
+                                </div>
+                            </Col>
+                        </Row>
+                    </form>
 
-                            <div className="form-group">
-                                <label htmlFor="clubAddress">Address</label>
-                                <input type="text" className="form-control" placeholder="Address" />
-                            </div>
-                        </form>
-                    </ModalBody>
+                    {/* action button */}
+                    <Button color="primary" type='submit' onClick={mode === 'update' ? this.props.onUpdateClub : this.props.onCreateClub}>Save</Button>
 
-                    <ModalFooter>
-                        <Button color="primary">Save</Button>
-                        <Button color="secondary" onClick={this.props.modalToggle}>Save</Button>
-                    </ModalFooter>
-                </Modal>
+                    <span className='btn-space-club'></span>
+
+                    <Button color="secondary" onClick={this.props.onCancel}><b>Cancel</b></Button>
+                </Card>
             </div>
         )
     }
