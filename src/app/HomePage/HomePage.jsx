130d15ebcf8e88cf7b5ad6bc59a26743c93c8cec
ahadsenja <hamdanilm96@gmail.com>
2019-04-14 20:23:51 +0800

    chore: update home page form #24

--- src/app/HomePage/HomePage.jsx
+++ src/app/HomePage/HomePage.jsx
@@ -15,60 +15,62 @@ class FrontPage extends React.Component {
                 <Navbar />
                 <JumbotronComponent />                
 
-                <section className='top-rank-section'>
-                    <Container>
-                        <h1>Top Rank</h1>
-                        <hr/>
-                        <Row>
-                            <Col>
-                                <Card className='top-rank'>
-                                    <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
-                                </Card>
-                            </Col>
+                <body>
+                    <section className='top-rank-section'>
+                        <Container>
+                            <h1>Top Rank</h1>
+                            <hr/>
+                            <Row>
+                                <Col>
+                                    <Card className='top-rank'>
+                                        <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
+                                    </Card>
+                                </Col>
 
-                            <Col>
-                                <Card className='top-rank'>
-                                    <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
-                                </Card>
-                            </Col>
+                                <Col>
+                                    <Card className='top-rank'>
+                                        <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
+                                    </Card>
+                                </Col>
 
-                            <Col>
-                                <Card className='top-rank'>
-                                    <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
-                                </Card>
-                            </Col>
+                                <Col>
+                                    <Card className='top-rank'>
+                                        <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
+                                    </Card>
+                                </Col>
 
-                            <Col>
-                                <Card className='top-rank'>
-                                    <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
-                                </Card>
-                            </Col>
-                        </Row>
-                    </Container>
-                </section>
+                                <Col>
+                                    <Card className='top-rank'>
+                                        <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </section>
 
-                <section className='community-section'>
-                    <div>
-                        <Row>
-                            <Col>
+                    <section className='community-section'>
+                        <div>
+                            <Row>
+                                <Col>
 
-                            </Col>
+                                </Col>
 
-                            <Col>
-                                <h3>Archery Community</h3>
-                                <p>Effortlessly host multiple tournaments, leagues and events for your loyal members.</p>
-                            </Col>
-                        </Row>
-                    </div>
-                </section>
+                                <Col>
+                                    <h3>Archery Community</h3>
+                                    <p>Effortlessly host multiple tournaments, leagues and events for your loyal members.</p>
+                                </Col>
+                            </Row>
+                        </div>
+                    </section>
 
-                <section className='sign-up-free-section'>
-                    <div align='center'>
-                        <h3><b>Providing the right tools for the gaming community</b></h3>
-                        <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
-                        <Button className='btn-signup-free' color='success'><b>Sign Up Free</b></Button>
-                    </div>
-                </section>
+                    <section className='sign-up-free-section'>
+                        <div align='center'>
+                            <h3><b>Providing the right tools for the gaming community</b></h3>
+                            <p>We aim to keep things simple, but there's plenty more to explore,including bracket predictions, voting, and themeable brackets for your website.</p>
+                            <Button className='btn-signup-free' color='success'><b>Sign Up Free</b></Button>
+                        </div>
+                    </section>
+                </body>
 
                 <FooterComponent />
             </div>
