a2b2c0b0e4760b078449b3eb4f397c44e9c7af4e
ahadsenja <hamdanilm96@gmail.com>
2019-04-19 14:22:28 +0800

    chore: update jumbotron space issue #26

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -12,10 +12,12 @@ class JumbotronComponent extends React.Component {
                 <Jumbotron>
                     <Container fluid>
                         <div align='center'>
+                            <br/>
                             <h1 className='display-5'><b>Made Your Archery Tournament Be Easy</b></h1>
                             <h4>This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</h4>
                             <hr/>
                             <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                            <br/>
                             <br/>
                             <a style={{textDecoration: 'none'}} className='btn-create-your-tournament' href="http://localhost:3000/#/tournament"><b>Create Your Tournament</b></a>
                         </div>
