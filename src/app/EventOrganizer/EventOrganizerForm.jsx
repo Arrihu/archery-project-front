23c3f3e0110d36a7a9440bc86ed08adc10f68db9
ahadsenja <hamdanilm96@gmail.com>
2019-04-26 19:39:41 +0800

    chore: update event organizer form issue #19

--- src/app/EventOrganizer/EventOrganizerForm.jsx
+++ src/app/EventOrganizer/EventOrganizerForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Card, Row, Col } from 'reactstrap'
 
-import './style/eventorganizer.scss'
+import './scss/eventorganizer.scss'
 
 
 class EventOrganizerForm extends React.Component {
