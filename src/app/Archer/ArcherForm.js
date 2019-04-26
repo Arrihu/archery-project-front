7712336ffbdbbe9d51eed8431e64bfe8eb852af9
ahadsenja <hamdanilm96@gmail.com>
2019-04-26 19:17:24 +0800

    feat: add archer form data issue #32

---
+++ src/app/Archer/ArcherForm.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Card, Button, Row, Col } from 'reactstrap'
+
+
+class ArcherForm extends React.Component {
+
+    render() {
+        const { name, gender, age } = this.props.dataState
+
+        return (
+            <div>
+                {/* <Card> */}
+                    <form action="">
+                        <Row>
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="name">Name</label>
+                                    <input type="text" name='name' className='form-control' placeholder='Name' value={name} onChange={this.props.handleChange} />
+                                </div>
+                            </Col>
+
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="gender">Gender</label>
+                                    <input type="text" name='gender' className='form-control' placeholder='Gender' value={gender} onChange={this.props.handleChange} />
+                                </div>
+                            </Col>
+
+                            <Col>
+                                <div className="form-group">
+                                    <label htmlFor="age">Age</label>
+                                    <input type="text" name='age' className='form-control' placeholder='Name' value={name} onChange={this.props.handleChange} />
+                                </div>
+                            </Col>
+                        </Row>
+                    </form>
+                {/* </Card> */}
+            </div>
+        )
+    }
+}
+
+export default ArcherForm
