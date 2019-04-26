d2b955388aa22a780c8fef4ec2e518ee4dbf2fa2
ahadsenja <hamdanilm96@gmail.com>
2019-04-26 19:18:28 +0800

    feat: add archer list data issue #33

---
+++ src/app/Archer/ArcherList.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { Table, Button, Card } from 'reactstrap'
+
+import ArcherTableHeader from './components/ArcherTableHeader'
+import Pagination from '../../components/Pagination/Pagination'
+import './scss/archer.scss'
+
+
+const ArcherList = ({ archers }) => {
+
+    return (
+        <div>
+            <Card>
+                <ArcherTableHeader />
+
+                <Table hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Age</th>
+                            <th>Club</th>
+                            <th>Tournament</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            typeof(archers) === 'object' &&
+                            archers.map((archer, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{i+1}</td>
+                                        <td>{archer.name}</td>
+                                        <td>{archer.gender}</td>
+                                        <td>{archer.age}</td>
+                                        <td>{archer.clubId}</td>
+                                        <td>{archer.tournamentId}</td>
+                                        <td>
+                                            <Button color='success' size='sm'>
+                                                <i className='cui-magnifying-glass'></i>
+                                            </Button>
+                                            <span></span>
+                                            <Button color='warning' size='sm'>
+                                                <i className='cui-note'></i>
+                                            </Button>
+                                            <span></span>
+                                            <Button color='danger' size='sm'>
+                                                <i className='cui-trash'></i>
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </Table>
+
+                <br />
+                <br />
+                <Pagination />
+            </Card>
+        </div>
+    )
+}
+
+export default ArcherList
